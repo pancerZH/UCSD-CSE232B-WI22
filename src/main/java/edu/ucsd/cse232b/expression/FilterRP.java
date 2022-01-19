2a34df1be24b3bff25c82d4cb7a882fa53e13ab5
Wenzhe Zhang <wenzheinfinity@gmail.com>
2022-01-19 11:30:27 -0800

    add filter relative path

--- src/main/java/edu/ucsd/cse232b/expression/FilterRP.java
+++ src/main/java/edu/ucsd/cse232b/expression/FilterRP.java
@@ -1,4 +1,39 @@
 package edu.ucsd.cse232b.expression;
 
+import java.util.Objects;
+
 public class FilterRP extends RelativePath {
+
+    final RelativePath rp;
+    final Filter ft;
+
+    public FilterRP(RelativePath r, Filter f) {
+        Objects.requireNonNull(r, "Relative path is NULL!");
+        Objects.requireNonNull(f, "Filter is NULL!");
+
+        this.rp = r;
+        this.ft = f;
+    }
+
+    @Override
+    public String toString() {
+        return this.rp.toString() + "(" + this.ft.toString() + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.rp, this.ft);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == this) {
+            return true;
+        }
+        if(null == o || this.getClass() != o.getClass()) {
+            return false;
+        }
+        FilterRP that = (FilterRP) o;
+        return Objects.equals(this.rp, that.rp) && Objects.equals(this.ft, that.ft);
+    }
 }
