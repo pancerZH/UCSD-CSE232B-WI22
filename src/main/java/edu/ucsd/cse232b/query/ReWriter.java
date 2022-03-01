d09f62678aebb21bc6947a0752c952738b3f4bbb
Wenzhe Zhang <wenzheinfinity@gmail.com>
2022-02-28 17:15:47 -0800

    add single rewriter

---
+++ src/main/java/edu/ucsd/cse232b/query/ReWriter.java
@@ -0,0 +1,96 @@
+package edu.ucsd.cse232b.query;
+
+import org.antlr.v4.runtime.misc.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReWriter {
+
+    // for [var] in [xq]
+    // map: var -> xq
+    private final Map<String, String> forMap;
+    // where [xq1] [cond] [xq2]
+    // map: <xq1, xq2> -> cond
+    private final Map<Pair<String, String>, String> condMap;
+
+    public ReWriter() {
+        this.forMap = new HashMap<>();
+        this.condMap = new HashMap<>();
+    }
+
+    public void setForMap(Map<String, String> map) {
+        this.forMap.clear();
+        this.forMap.putAll(map);
+    }
+
+    public void setCondMap(Map<Pair<String, String>, String> map) {
+        this.condMap.clear();
+        this.condMap.putAll(map);
+    }
+
+    private String convertForClause() {
+        StringBuilder sb = new StringBuilder("for ");
+        for(Map.Entry<String, String> entry : this.forMap.entrySet()) {
+            sb.append(entry.getKey());
+            sb.append(" ");
+            sb.append("in");
+            sb.append(" ");
+            sb.append(entry.getValue());
+            sb.append(",\n");
+        }
+        sb.deleteCharAt(sb.length()-2);  // remove last comma
+        return sb.toString();
+    }
+
+    private String convertCondClause() {
+        // to see whether we can push down any selection
+        StringBuilder sb = new StringBuilder("where ");
+        boolean pushDown = false;
+        for(Map.Entry<Pair<String, String>, String> entry : this.condMap.entrySet()) {
+            String condRoot1 = entry.getKey().a.split("/")[0];
+            String condRoot2 = entry.getKey().b.split("/")[0];
+            if((this.forMap.containsKey(condRoot1) && this.forMap.containsKey(condRoot2)) ||
+                    (this.forMap.containsKey(condRoot1) && !condRoot2.startsWith("$")) ||
+                    (this.forMap.containsKey(condRoot2) && !condRoot1.startsWith("$"))) {
+                if(!pushDown) {
+                    pushDown = true;
+                }
+                sb.append(entry.getKey().a);
+                sb.append(" ");
+                sb.append(entry.getValue());
+                sb.append(" ");
+                sb.append(entry.getKey().b);
+                sb.append(",\n");
+            }
+        }
+        if(pushDown) {
+            sb.deleteCharAt(sb.length()-2);  // remove last comma
+            return sb.toString();
+        } else {
+            return "";
+        }
+    }
+
+    private String convertReturnClause() {
+        StringBuilder sb = new StringBuilder("return ");
+        sb.append("<tuple>\n");
+        for(String var : this.forMap.keySet()) {
+            String rawName = var.substring(1);
+            sb.append("<").append(rawName).append(">");
+            sb.append(var);
+            sb.append("<").append("\\").append(rawName).append(">");
+            sb.append(",\n");
+        }
+        sb.deleteCharAt(sb.length()-2);  // remove last comma
+        sb.append("<\\tuple>,\n");
+        return sb.toString();
+    }
+
+    public String convertToString() {
+        return this.convertForClause() +
+                this.convertCondClause() +
+                this.convertReturnClause();
+    }
+
+}
---
+++ src/test/java/edu/ucsd/cse232b/query/ReWriterTest.java
@@ -0,0 +1,48 @@
+package edu.ucsd.cse232b.query;
+
+import org.antlr.v4.runtime.misc.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReWriterTest {
+
+    @org.junit.jupiter.api.Test
+    void convertToString() {
+        ReWriter reWriter = new ReWriter();
+        Map<String, String> forMap = new HashMap<>();
+        forMap.put("$b1", "doc(\"input\")/book");
+        forMap.put("$aj", "$b1/author/first/text()");
+        forMap.put("$a1", "$b1/author");
+        forMap.put("$af1", "$a1/first");
+        forMap.put("$al1", "$a1/last");
+
+        Map<Pair<String, String>, String> condMap = new HashMap<>();
+        condMap.put(new Pair<>("$aj", "John"), "eq");
+        condMap.put(new Pair<>("$af1", "$af21"), "eq");
+        condMap.put(new Pair<>("$al1", "$al21"), "eq");
+
+        reWriter.setForMap(forMap);
+        reWriter.setCondMap(condMap);
+
+        String expectedRes = """
+                for $af1 in $a1/first,
+                $b1 in doc("input")/book,
+                $al1 in $a1/last,
+                $a1 in $b1/author,
+                $aj in $b1/author/first/text()
+                where $aj eq John
+                return <tuple>
+                <af1>$af1<\\af1>,
+                <b1>$b1<\\b1>,
+                <al1>$al1<\\al1>,
+                <a1>$a1<\\a1>,
+                <aj>$aj<\\aj>
+                <\\tuple>,
+                """;
+
+        assertEquals(expectedRes, reWriter.convertToString());
+    }
+}
